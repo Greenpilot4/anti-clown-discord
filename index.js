831f2ba197c8975febd072558746620258efa5c2
ギャビンコラード <dev.greenpilot@gmail.com>
2020-11-08 04:13:21 +0000

    changed logging

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const {
-    prefix,
-    token,
-    rmessage
-} = require("./config.json");
-
 const client = new Discord.Client();
+const { prefix, token, rmessage } = require("./config.json");
+
+const log = require("./events/log.js");
+const currentTime = require("./events/time.js");
 
 client.once("ready", () => {
-    console.log(`${client.user.username} Ready!`);
+    log(currentTime() + " " + `${client.user.tag} online!`)
 });
 
-client.on("message", async (message) => {
+client.on("message", async(message) => {
     if (message.author.bot) return;
 
-    try {
-        if (message.content.includes("🤡")) {
-            message.delete()
-            message.reply(rmessage)
-                .then(msg => {
+    if (message.content.includes("🤡")) {
+        log(currentTime() + " " + message.author.tag + " sent a clown");
+        log(currentTime() + " " + message.author.tag + "'s" + " clown deleted")
+        message.delete()
+        message.reply(rmessage)
+            .then(msg => {
                 msg.delete({ timeout: 3000 });
             })
-        }
-            
-    } catch (error) {
-        console.error(error);
-        message.reply('Something Went Wrong!');
+    }
+
+});
+
+client.on("messageReactionAdd", async(reaction, user) => {
+
+    if (reaction.emoji.name == "🤡") {
+        log(currentTime() + " " + reaction.user.username + " reacted with a clown");
+        log(currentTime() + " " + reaction.user.username + "'s" + " clown reaction deleted")
+        reaction.remove(user)
     }
 });
 
-client.on("messageReactionAdd", async (reaction, user) => {
-   
-   if (reaction.emoji.name == "🤡") {
-       reaction.remove(user)
-   }
-}); 
 client.login(token);
